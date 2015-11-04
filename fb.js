821c038da51e13160e8d55470341876a22dc7e60
Cem Gencer <cem.gencer@me.com>
2015-11-04 12:55:41 +0200

    ...

--- fb.js
+++ fb.js
@@ -2,5 +2,5 @@
 module.exports = {
 	'appID' : '412315785625651',
 	'appSecret' : '7451ddd714e722e855902b3c333d5c70',
-	'callbackUrl' : 'http://localhost:3000/login/facebook/callback'
+	'callbackUrl' : 'http://78.176.137.129:3000/login/facebook/callback'
 }
